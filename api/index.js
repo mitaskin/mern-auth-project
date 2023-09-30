fe39ea5561471226503b2bc03c1f7c2ee13df99d
M.ikbal TAŞKIN <mi.taskin027@gmail.com>
2023-09-30 19:51:03 +0300

    create user model

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log(err);
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
