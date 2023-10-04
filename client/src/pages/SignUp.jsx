5d70a598a284b0f9df536eac3bce703449d8bde9
M.ikbal TAŞKIN <mi.taskin027@gmail.com>
2023-10-04 21:59:35 +0300

    complete sign up page functionalty

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,19 +4,36 @@ import { useState } from "react";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    const res = await axios
-      .post("http://localhost:3000/api/auth/signup", formData,)
 
-    console.log(res);
-  }
+    try {
+      setLoading(true);
+      setError(false);
+
+      const res = await axios.post(
+        "http://localhost:3000/api/auth/signup",
+        formData
+      );
+
+      const data = await res.json();
+
+      if (data.success == false) {
+        setError(false);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -45,8 +62,11 @@ export default function SignUp() {
           placeholder="Password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-40">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-40"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
@@ -56,6 +76,7 @@ export default function SignUp() {
           <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong"}</p>
     </div>
   );
 }
